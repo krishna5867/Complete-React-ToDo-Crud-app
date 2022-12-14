9e38c38867d04ca0fbe49a4e3f5d5bae5d916be4
Anirudh Jwala <anirudhsai2@gmail.com>
2022-12-14 22:10:48 +0530

    Never commit node_modules to github

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const { 
-        home,
-        createTodo,
-        getTodos,
-        editTodo,
-        deleteTodo,
-    } = require('../controllers/createTodos')
+const {
+  home,
+  createTodo,
+  getTodos,
+  editTodo,
+  deleteTodo,
+} = require("../controllers/createTodos");
 
 const router = express.Router();
 
-router.get('/', home)
-router.post('/createTodo', createTodo);
-router.get('/getTodos', getTodos)
-// router.put('/editTodo/:id', editTodo)
-router.delete('/deleteTodo/:id', deleteTodo)
-
+router.get("/", home);
+router.post("/createTodo", createTodo);
+router.get("/getTodos", getTodos);
+router.put("/editTodo/:id", editTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
 
 module.exports = router;
