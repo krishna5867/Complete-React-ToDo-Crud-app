82979082b1a70c15b65fad03d68324a516fa8a27
Anirudh Jwala <anirudhsai2@gmail.com>
2023-01-05 21:14:12 +0530

    Code fixes for react project

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Container, Card, CardBody, Input } from "reactstrap";
 
-const Todo = ({BASE_URL,fetchTodosData}) => {
+const Todo = ({ fetchTodosData }) => {
   const [title, setTitle] = useState("");
   const [tasks, setTasks] = useState("");
 
@@ -13,9 +13,8 @@ const Todo = ({BASE_URL,fetchTodosData}) => {
         tasks: tasks,
       };
 
-      const res = await axios.post(`${BASE_URL}/createTodo`, todo);
+      const res = await axios.post(`/createTodo`, todo);
       if (res.data.success) {
-        console.log("Title created successfully");
         setTitle("");
         setTasks("");
         fetchTodosData();
@@ -32,55 +31,53 @@ const Todo = ({BASE_URL,fetchTodosData}) => {
 
     if ((title || tasks) === "") {
       alert("Todo Field can't be Empty");
-    } else {
-      console.log("");
     }
   };
 
   return (
-    <div>
-      <Container className="mt-4" style={{ width: "78rem" }}>
-        <Card>
-          <CardBody>
-            <Container className="mb-4">
-              <form onSubmit={handleSubmit}>
-                <h3 className="mt-2"><b> Todo App</b></h3>
-                <CardBody className="mt-2 d-flex">
-                  <Input
-                    type="text"
-                    id="title"
-                    name="title"
-                    value={title}
-                    placeholder="Todo Title"
-                    onChange={(e) => {
-                      setTitle(e.target.value);
-                    }}
-                  />
-                </CardBody>
-                <CardBody>
-                  <Input
-                    type="text"
-                    id="task"
-                    name="task"
-                    value={tasks}
-                    placeholder="Task Name"
-                    onChange={(e) => {
-                      setTasks(e.target.value);
-                    }}
-                  />
-                  <button
-                    className="btn btn-success btn-lg col-12  mt-4"
-                    type="Submit"
-                  >
-                    Add Todo
-                  </button>
-                </CardBody>
-              </form>
-            </Container>
-          </CardBody>
-        </Card>
-      </Container>
-    </div>
+    <Container className="mt-4 mb-4" style={{ width: "78rem" }}>
+      <Card>
+        <CardBody>
+          <Container className="mb-4">
+            <form onSubmit={handleSubmit}>
+              <h3 className="mt-2">
+                <b> Todo App</b>
+              </h3>
+              <CardBody className="mt-2 d-flex">
+                <Input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={title}
+                  placeholder="Todo Title"
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                  }}
+                />
+              </CardBody>
+              <CardBody>
+                <Input
+                  type="text"
+                  id="task"
+                  name="task"
+                  value={tasks}
+                  placeholder="Task Name"
+                  onChange={(e) => {
+                    setTasks(e.target.value);
+                  }}
+                />
+                <button
+                  className="btn btn-success btn-lg col-12  mt-4"
+                  type="Submit"
+                >
+                  Add Todo
+                </button>
+              </CardBody>
+            </form>
+          </Container>
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
